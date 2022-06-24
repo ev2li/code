f37a8b17130399d77673b0802b6bb0efe34958d9
ev2li <ev2li9748@163.com>
2022-06-24 22:43:23 +0800

    vector

--- ccplus2/c++/stl/vector/vector_reserve.cc
+++ ccplus2/c++/stl/vector/vector_reserve.cc
@@ -22,6 +22,8 @@ void printVector(vector<int>& v){
 
 void test01(){
         vector<int> v;
+        //利用reserve预留空间
+        v.reserve(100000);
         int num = 0; //统计开辟次数
         int *p = NULL;
         for(int i = 0; i < 100000; i++){
